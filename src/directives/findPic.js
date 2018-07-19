fb93536e1d4812de46e7861f8cfd62bd18ac8f16
蔡荣庚 <89987112@qq.com>
2018-07-19 17:00:39 +0800

    screenshot html 雏形

--- src/directives/findPic.js
+++ src/directives/findPic.js
@@ -3,16 +3,24 @@ const storage = require("../js/storage")
 const keyboard = require("../js/keyboard")
 const fse = require("fs-extra");
 const path = require("path");
+const {BrowserWindow} = require('electron');
 
 /**
  * 根据屏幕截图快速在指定目录中查找到相似图片并列出
+ * 
+ * test
+ *      
+ *       $findPic
  */
 module.exports = {
-    validate:/^\$findPic\?(.+)/,
+    validate:/^\$findPic\??(.*)/,
     handle(result){
         let global = storage.storage||{};
         let [name,params={}] = utils.parseLineToObject(result[0]);
         let {url,file} = params;
+        
+        let win = new BrowserWindow({width: 800, height: 600,frame:false,center:true});
+        win.loadFile(path.resolve("./src/views/screenshot.html"));
 
         // const imghash = require('imghash');
         // const hamming = require('hamming-distance');
