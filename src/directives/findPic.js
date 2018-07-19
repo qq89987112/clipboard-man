09b5357f03e72db344a1bc7bc00f9e270b76964c
蔡荣庚 <89987112@qq.com>
2018-07-19 16:14:39 +0800

    update

--- src/directives/findPic.js
+++ src/directives/findPic.js
@@ -13,6 +13,23 @@ module.exports = {
         let global = storage.storage||{};
         let [name,params={}] = utils.parseLineToObject(result[0]);
         let {url,file} = params;
-        
+
+        // const imghash = require('imghash');
+        // const hamming = require('hamming-distance');
+
+        // imghash.hashraw
+        // const hash1 = imghash.hash('./image1.png');
+        // const hash2 = imghash.hash('./image2.png');
+
+        // Promise.all([hash1, hash2]).then((results) => {
+        //     const dist = hamming(results[0], results[1]);
+        //     console.log(`Distance between images is: ${dist}`);
+        //     if (dist <= 20) {
+        //     console.log('Images are similar');
+        //     } else {
+        //     console.log('Images are NOT similar');
+        //     }
+        // });
+
     }
 }
